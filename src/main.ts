b27e48976919e738265266b70a97691db96517fa
George <geocoblisan@gmail.com>
2022-10-31 17:12:40 +0200

    deploy test commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { Pool, Client } = require('pg')
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: 'http://localhost:4200'});
@@ -9,3 +11,15 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
+var client = new Client({
+  user: "postgres",
+  password: "george",
+  database: "EnergyUtility",
+  port: 5432,
+  host: "localhost",
+  ssl: true
+});
+client.connect(function () {
+  console.log("connected");
+});
