05c343f9b5e570e4e4fc5b5882ec1100c027a373
George <geocoblisan@gmail.com>
2022-10-31 16:35:01 +0200

    deploy test commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: 'http://localhost:4200'});
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.port || 3000);
 }
 bootstrap();
